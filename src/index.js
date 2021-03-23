20394ecc0bd74e46259a85027e6af2dc8d95659a
Alessandro Gottardi <alegottardi@posteo.net>
2021-03-23 21:46:49 +0100

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./screens/Home"
+import Registration from "./screens/Registration";
+import Login from "./screens/Login";
+import News from "./screens/News";
+import MyStocks from "./screens/MyStocks";
+import SingleNews from "./screens/SingleNews";
+import SingleStock from "./screens/SingleStock";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Switch>
+              <Route path="/login">
+                  <Login />
+              </Route>
+              <Route path="/registration">
+                  <Registration />
+              </Route>
+              <Route path="/news">
+                  <News />
+              </Route>
+              <Route path="/mystock">
+                  <MyStocks />
+              </Route>
+              <Route path="/singleNews/:id">
+                  <SingleNews />
+              </Route>
+              <Route path="/singleStock/:id">
+                  <SingleStock />
+              </Route>
+              <Route path="/">
+                  <Home />
+              </Route>
+          </Switch>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
