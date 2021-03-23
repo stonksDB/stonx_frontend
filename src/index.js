4b64d708f1e89be1b34b1fdd065363647229cada
Luca Taddeo <ltaddeo@unibz.it>
2021-03-24 00:49:22 +0100

    Initial Theming implementation

--- src/index.js
+++ src/index.js
@@ -1,42 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Home from "./screens/Home"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./screens/Home";
 import Registration from "./screens/Registration";
 import Login from "./screens/Login";
 import News from "./screens/News";
 import MyStocks from "./screens/MyStocks";
 import SingleNews from "./screens/SingleNews";
 import SingleStock from "./screens/SingleStock";
+import { CssBaseline, MuiThemeProvider} from "@material-ui/core";
+import lightTheme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
+    <MuiThemeProvider theme={lightTheme}>
+      <CssBaseline />
       <Router>
-          <Switch>
-              <Route path="/login">
-                  <Login />
-              </Route>
-              <Route path="/registration">
-                  <Registration />
-              </Route>
-              <Route path="/news">
-                  <News />
-              </Route>
-              <Route path="/mystock">
-                  <MyStocks />
-              </Route>
-              <Route path="/news/:id">
-                  <SingleNews />
-              </Route>
-              <Route path="/stock/:id">
-                  <SingleStock />
-              </Route>
-              <Route path="/">
-                  <Home />
-              </Route>
-          </Switch>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/registration">
+            <Registration />
+          </Route>
+          <Route path="/news">
+            <News />
+          </Route>
+          <Route path="/mystock">
+            <MyStocks />
+          </Route>
+          <Route path="/news/:id">
+            <SingleNews />
+          </Route>
+          <Route path="/stock/:id">
+            <SingleStock />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </Router>
+    </MuiThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,18 @@
+import { Button, Card, CardActionArea, CardContent, Typography } from "@material-ui/core";
 import "../App.css";
 
 export default function Home() {
   return (
     <div className="App">
+      <Card>
+          <CardContent>
+            <Typography>Ciao</Typography>
+          </CardContent>
+      </Card>
       <p>This is Home</p>
+      <Button variant="contained" color="primary">
+        Test
+      </Button>
     </div>
   );
 }
