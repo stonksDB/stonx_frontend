04a4c97dd3ceb33a554219f99815876a9cb43a34
Alessandro Gottardi <alegottardi@posteo.net>
2021-03-25 17:43:26 +0100

    refactor: simplify routes management

--- src/index.js
+++ src/index.js
@@ -2,39 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Home from "./screens/Home"
-import Registration from "./screens/Registration";
-import Login from "./screens/Login";
-import News from "./screens/News";
-import MyStocks from "./screens/MyStocks";
-import SingleNews from "./screens/SingleNews";
-import SingleStock from "./screens/SingleStock";
+import routes from "./routes"
+
 
 ReactDOM.render(
   <React.StrictMode>
       <Router>
           <Switch>
-              <Route path="/login">
-                  <Login />
-              </Route>
-              <Route path="/registration">
-                  <Registration />
-              </Route>
-              <Route path="/news">
-                  <News />
-              </Route>
-              <Route path="/mystock">
-                  <MyStocks />
-              </Route>
-              <Route path="/news/:id">
-                  <SingleNews />
-              </Route>
-              <Route path="/stock/:id">
-                  <SingleStock />
-              </Route>
-              <Route path="/">
-                  <Home />
-              </Route>
+              {routes().map(({name, path, icon, page}) => (
+                <Route key={path} path={path}>
+                    {page}
+                </Route>
+              ))}
           </Switch>
       </Router>
   </React.StrictMode>,
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import {
+  CreditCard as MyStockIcon,
+  Equalizer as StockIcon,
+  Home as HomeIcon,
+  ImportContacts as NewsIcon
+} from "@material-ui/icons";
+import Login from "./screens/Login";
+import Registration from "./screens/Registration";
+import SingleStock from "./screens/SingleStock";
+import SingleNews from "./screens/SingleNews";
+import News from "./screens/News";
+import MyStocks from "./screens/MyStocks";
+import Home from "./screens/Home";
+import React from "react";
+
+export default function routes() {
+  return [
+    {name: "Login", path: "/login", icon: <HomeIcon/>, page: <Login/>},
+    {name: "Registration", path: "/registration", icon: <HomeIcon/>, page: <Registration/>},
+    {name: "Single Stock", path: "/stock/:id", icon: <StockIcon/>, page: <SingleStock/>},
+    {name: "Single News", path: "/news/:id", icon: <NewsIcon/>, page: <SingleNews/>},
+    {name: "News", path: "/news", icon: <NewsIcon/>, page: <News/>},
+    {name: "My Stocks", path: "/mystock", icon: <MyStockIcon/>, page: <MyStocks/>},
+    {name: "Home", path: "/", icon: <HomeIcon/>, page: <Home/>},
+  ];
+}
