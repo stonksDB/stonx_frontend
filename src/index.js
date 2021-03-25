039386909336a2ec1743e62256a8939b07a2d73a
Alessandro Gottardi <alegottardi@posteo.net>
2021-03-25 15:01:23 +0100

    fix: working routes with github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Registration from "./screens/Registration";
 import Login from "./screens/Login";
