d0d1d4643cede1e7a7e13e0002fc41d529e8387b
Luca Taddeo <lucataddeo2000@gmail.com>
2021-03-28 15:59:25 +0200

    NavBarMobile: add login and register button

--- src/components/navbar/NavBarMobile.js
+++ src/components/navbar/NavBarMobile.js
@@ -2,14 +2,47 @@ import { useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import React from "react";
 import { NavbarList } from "./NavBar";
+import { Button } from "@material-ui/core";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router";
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    button: {
+      ...theme.button,
+      marginBottom: 10,
+      padding: 4,
+    },
+    buttonContainer: {
+      position: "absolute",
+      bottom: 0,
+      marginBottom: 10,
+      width: "100%",
+      paddingRight: 10,
+      paddingLeft: 10,
+    },
+  })
+);
 
 const NavBarMobile = (props) => {
-  const drawer = <NavbarList className={props.className} />;
+  const drawer = <NavbarList className={props.className} {...props} />;
   const { window } = props;
   const container =
     props.window !== undefined ? () => window().document.body : undefined;
   const theme = useTheme();
+  const classes = useStyles();
+
+  const history = useHistory();
+
+  const goLogin = () => {
+    let path = "/login";
+    history.push(path);
+  };
+
+  const goRegister = () => {
+    let path = "/register";
+    history.push(path);
+  };
 
   return (
     <Drawer
@@ -23,6 +56,26 @@ const NavBarMobile = (props) => {
       }}
     >
       {drawer}
+      {!props.isLoggedIn && (
+        <div className={classes.buttonContainer}>
+          <Button
+            className={classes.button}
+            onClick={goLogin}
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+          <Button
+            className={classes.button}
+            onClick={goRegister}
+            variant="outlined"
+            color="primary"
+          >
+            Register
+          </Button>
+        </div>
+      )}
     </Drawer>
   );
 };
