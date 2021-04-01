b03f0bd72a74f0f1694561f6143620b8832cdff8
Alessandro Gottardi <alegottardi@posteo.net>
2021-04-01 11:56:48 +0200

    build: fix graph animations responsiveness

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,13 @@
 const { useBabelRc, override } = require('customize-cra')
 
-module.exports = override(
-  useBabelRc()
-);
+module.exports = function override(config, env) {
+  useBabelRc();
+
+  //See https://github.com/plouc/nivo/issues/1290
+  config.module.rules.push({
+      test: /react-spring/,
+      sideEffects: true,
+    })
+
+  return config;
+}
