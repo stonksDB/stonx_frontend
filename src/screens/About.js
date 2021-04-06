9cff6b30b941e67cc755a2488376c630d80807e9
Luca Taddeo <lucataddeo2000@gmail.com>
2021-04-06 19:19:18 +0200

    About: create members' Cards

--- src/screens/About.js
+++ src/screens/About.js
@@ -4,19 +4,57 @@ import {
   Divider,
   Grid,
   makeStyles,
+  Paper,
   Typography,
 } from "@material-ui/core";
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import logo_webp from "../assets/logo_header/logo_header.webp";
+// import sebastian from "../assets/team/sebastian.jpeg";
+// import alessandro from "../assets/team/alessandro.jpeg";
+// import riccardo from "../assets/team/riccardo.jpeg";
+import luca from "../assets/team/luca.jpeg";
+
 import LogosCarousel from "../components/Carousel";
+
+const team = [
+  {
+    name: "Sebastian Cavada",
+    role: "Back End Developer",
+    quote: "I like stocks",
+    image: "sebastian",
+  },
+  {
+    name: "Alessandro Gottardi",
+    role: "Front End Developer",
+    quote: "This is a very smart quote",
+    image: "alessandro",
+  },
+  {
+    name: "Riccardo Rigoni",
+    role: "Back End Developer",
+    quote: "This is a very smart quote",
+    image: "riccardo",
+  },
+  {
+    name: "Luca Taddeo",
+    role: "Front End Developer",
+    quote: "This is a very smart quote",
+    image: luca,
+  },
+];
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     pageTitle: {
       paddingBottom: theme.spacing(3),
     },
-    card: theme.card,
+    card: {
+      ...theme.card,
+      textAlign: "center",
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+    },
     container: {
       paddingRight: theme.spacing(5),
       paddingLeft: theme.spacing(5),
@@ -32,6 +70,24 @@ const useStyles = makeStyles((theme) =>
       marginBottom: theme.spacing(4),
     },
     button: { ...theme.button, marginTop: theme.spacing(5) },
+    memberImage: {
+      width: "75%",
+      borderRadius: 15,
+      marginBottom: theme.spacing(2),
+    },
+    memberRole: {
+      textTransform: "uppercase",
+      fontSize: 11,
+      lineHeight: 1,
+    },
+    memberName: {
+      fontWeight: "bold",
+      fontSize: 18,
+    },
+    memberQuote: {
+      fontStyle: "italic",
+      fontSize: 14,
+    },
   })
 );
 
@@ -46,7 +102,7 @@ const About = (props) => {
 
   return (
     <Grid container justify="center">
-      <Grid item xs={12} sm={9} lg={7} className={classes.container}>
+      <Grid item xs={12} sm={10} lg={8} className={classes.container}>
         <Grid container justify="center">
           <Grid
             item
@@ -89,14 +145,28 @@ const About = (props) => {
           <Typography variant={"h5"} className={classes.title}>
             The Awesome Team
           </Typography>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tortor
-            justo, laoreet at congue vitae, ultricies ac purus. Proin rhoncus
-            cursus facilisis. Sed vitae elit mattis, dictum enim eu, auctor
-            ante. Phasellus sit amet dui nibh. Maecenas rhoncus augue in erat
-            consequat, in commodo lacus mattis. Suspendisse eget urna mollis,
-            varius leo non, sagittis orci.
-          </Typography>
+          <Grid container spacing={6} style={{marginTop: 1}}>
+            {team.map((member) => (
+              <Grid item xs={6} md={3}>
+                <Paper elevation={0} className={classes.card}>
+                  <img
+                    src={member.image}
+                    className={classes.memberImage}
+                    alt={member.name}
+                  />
+                  <Typography className={classes.memberRole}>
+                    {member.role}
+                  </Typography>
+                  <Typography className={classes.memberName}>
+                    {member.name}
+                  </Typography>
+                  <Typography className={classes.memberQuote}>
+                    {member.quote}
+                  </Typography>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
         </div>
         <Divider />
 
