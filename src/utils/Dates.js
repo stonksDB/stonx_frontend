7d14b821b04c3b4ed301ab33c5e68c4be57262ad
alegotta <alegottardi@posteo.net>
2021-06-05 10:45:40 +0200

    fix: date parsing

--- src/utils/Dates.js
+++ src/utils/Dates.js
@@ -1,3 +1,11 @@
 export function parseDate(date, format="medium") {
-  return new Intl.DateTimeFormat("default", {dateStyle: format}).format(new Date(date));
+  let dateObj;
+  if (date.includes("-")) {   // YYYY-MM-DD format
+    const split = date.split("-");
+    dateObj = new Date(Date.UTC(split[2], split[1]-1, split[0]));
+  } else {
+    dateObj = new Date(date);
+  }
+
+  return new Intl.DateTimeFormat("default", {dateStyle: format}).format(dateObj);
 }
